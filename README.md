986c1ae31204b8862c47669d1970659a67da46ee
jslyonnais <jslyonnais@devjs.io>
2023-08-08 20:24:50 -0400

    add conditions

---
+++ README.md
@@ -0,0 +1,36 @@
+# Manga CBZ Processor
+
+Python tool optimized for manga `.cbz` files. It efficiently compresses, resizes, and renames files, ensuring optimal storage and clarity while preserving the essence of your favorite manga.
+
+## Features
+
+1. **Compression**: The script compresses the `.cbz` files to reduce their file size.
+2. **Resizing**: Images inside the `.cbz` files are resized if their height is greater than a set limit (e.g., 1024 pixels) while preserving their aspect ratio.
+3. **Renaming**: The script renames files to remove any content inside parentheses from the filename.
+
+## Requirements
+
+- Python 3.x
+- Pillow library: `pip install Pillow`
+
+## Usage
+
+1. Clone the repository:
+
+   ```bash
+   git clone [your-repo-link]
+   cd [repository-name]
+
+   ```
+
+1. Ensure you have the Pillow library installed:
+
+   ```bash
+   pip install Pillow
+   ```
+
+1. Run the script:
+
+   ```bash
+   python cbz_processor.py
+   ```
---
+++ script.py
@@ -0,0 +1,63 @@
+import os
+import re
+import zipfile
+from PIL import Image
+import tempfile
+
+def compress_cbz(file_path, output_path=None, quality=80, max_height=1024):
+    if output_path is None:
+        output_path = file_path
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # Extract all files to a temp directory
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall(temp_dir)
+
+        # Compress and optionally resize each image
+        for root, _, files in os.walk(temp_dir):
+            for file in files:
+                if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                    img_path = os.path.join(root, file)
+                    with Image.open(img_path) as img:
+                        # Check if the image height is greater than the max height
+                        if img.height > max_height:
+                            aspect_ratio = img.width / img.height
+                            new_width = int(aspect_ratio * max_height)
+                            img = img.resize((new_width, max_height), Image.ANTIALIAS)
+                        
+                        img.save(img_path, "JPEG", quality=quality)
+
+        # Create a new CBZ with the compressed (and optionally resized) images
+        with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for root, _, files in os.walk(temp_dir):
+                for file in files:
+                    img_path = os.path.join(root, file)
+                    arcname = os.path.relpath(img_path, temp_dir)
+                    zipf.write(img_path, arcname)
+
+    print(f"Compressed {file_path}")
+
+def remove_parenthesis_content(filename):
+    # This regex pattern matches anything inside parentheses, including the parentheses themselves.
+    pattern = r'\(.*?\)'
+    return re.sub(pattern, '', filename).strip()
+
+def process_files(directory):
+    # Iterate over each file in the directory
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        if filename.endswith('.cbz'):
+            # Compress the .cbz file
+            compress_cbz(filepath)
+            
+            # Rename the file after removing content inside parentheses
+            new_filename = remove_parenthesis_content(filename)
+            new_file_path = os.path.join(directory, new_filename)
+            
+            if new_file_path != filepath:
+                os.rename(filepath, new_file_path)
+                print(f'Renamed {filename} to {new_filename}')
+
+# Specify the directory containing the files to process
+directory_path = "./files/"
+process_files(directory_path)
